8efdc3a95c6372d7d8427974a8d5e0890d89f270
abdelilah <abdouajjouqa@gmail.com>
2026-01-30 12:27:47 +0100

    feat: add MongoDB configuration with Mongoose and update dependencies

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { mongodbConfig } from './config/mongodbConfig';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: mongodbConfig,
+      inject: [ConfigService]
+    }),
+  ],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule { }
---
+++ backend/src/config/mongodbConfig.ts
@@ -0,0 +1,6 @@
+import { ConfigService } from "@nestjs/config";
+import { MongooseModuleOptions } from "@nestjs/mongoose";
+
+export const mongodbConfig = async (configService: ConfigService): Promise<MongooseModuleOptions> => ({
+    uri: configService.get<string>('MONGODB_URI')
+})
